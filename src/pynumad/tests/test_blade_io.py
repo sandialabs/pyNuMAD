c3035b54ae05bddcce07d0f0d7f653a9131642b4
kbonney <kirkb1998@gmail.com>
2023-07-11 16:55:14 -0400

    small path fix

--- src/pynumad/tests/test_blade_io.py
+++ src/pynumad/tests/test_blade_io.py
@@ -9,7 +9,7 @@ test_data_dir = DATA_PATH
 class TestBladeIO(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.xlsxfile = join(test_data_dir,"Excel2ObjectExample.xlsx")
+        self.xlsxfile = join(test_data_dir,"blade_excels","Excel2ObjectExample.xlsx")
         self.yamlfile = join(test_data_dir,"blade_yamls","BAR1_SNL_1_18_2021.yaml")
         
     def test_xlsx_blade(self):
