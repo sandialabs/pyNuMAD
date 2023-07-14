9b0903266251466cfe0db9386ab204aa233cd627
kbonney <kirkb1998@gmail.com>
2023-07-14 12:02:28 -0400

    documentation update

--- docs/conf.py
+++ docs/conf.py
@@ -1,53 +1,30 @@
 # -*- coding: utf-8 -*-
-#
-# Configuration file for the Sphinx documentation builder.
-#
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
 # -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('../'))
-# sys.path.insert(0, os.path.abspath('../pynumad'))
-# sys.path.insert(0, os.path.abspath("../pynumad/utils"))
-
-
 
 # -- Project information -----------------------------------------------------
 
 project = 'PyNuMAD'
-copyright = '2022, Kirk Bonney'
+copyright = '2023, Kirk Bonney'
 author = 'Kirk Bonney'
 
-# The short X.Y version
-version = ''
-# The full version, including alpha/beta/rc tags
+version = '1.0'
 release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     # 'sphinx.ext.viewcode', # commenting out for now b/c bad render width
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinxcontrib.bibtex',
 ]
 napoleon_use_rtype = False
 viewcode_import = True
@@ -56,6 +33,7 @@ numpydoc_show_inherited_class_members = False
 numpydoc_class_members_toctree = False
 autodoc_member_order = 'bysource'
 autoclass_content = 'both'
+bibtex_bibfiles = ['refs/publications.bib','refs/conclusion.bib']
 
 templates_path = ['_templates']
 
@@ -68,11 +46,10 @@ language = "en"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_user']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -81,28 +58,11 @@ pygments_style = 'sphinx'
 #
 html_theme = 'sphinx_rtd_theme'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_style = 'css/my_style.css'
-
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# The default sidebars (for documents that don't match any pattern) are
-# defined by theme itself.  Builtin themes are using these templates by
-# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-# 'searchbox.html']``.
-#
-# html_sidebars = {}
-
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
@@ -138,7 +98,6 @@ latex_documents = [
      'Kirk Bonney', 'manual'),
 ]
 
-
 # -- Options for manual page output ------------------------------------------
 
 # One entry per manual page. List of tuples
@@ -147,7 +106,6 @@ man_pages = [
     (master_doc, 'pynumad', 'PyNuMAD Documentation',
      [author], 1)
 ]
-
 
 # -- Options for Texinfo output ----------------------------------------------
 
