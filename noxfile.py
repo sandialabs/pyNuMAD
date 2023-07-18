2c598bb723c8626ea2e866ab9bfbad9cd9c9419c
Kirk Bonney <47759761+kbonney@users.noreply.github.com>
2023-07-18 12:11:03 -0400

    CICD updates
    Added noxfile for task running. Added workflow for building and serving docs

---
+++ noxfile.py
@@ -0,0 +1,28 @@
+import nox
+
+@nox.session
+def tests(session):
+    session.install(".")
+    session.install("pytest")
+    session.run("pytest")
+
+@nox.session
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", "--import-order-style", "google")
+    
+@nox.session
+def docs(session):
+    session.install(".")
+    session.install("sphinx")
+    session.install("sphinx_rtd_theme")
+    session.install("sphinxcontrib-bibtex")
+    session.run("sphinx-build", "-M", "html", "docs/", "docs/_build")
+
+@nox.session
+def serve(session):
+    session.run("python", "-m", "http.server", "-b", "localhost", "-d", "docs/_build/html", "8085")
+   
+@nox.session
+def check_style(session):
+    session.run("black", "--check")
