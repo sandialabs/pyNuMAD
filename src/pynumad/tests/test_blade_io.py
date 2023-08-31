ed686e4cb505dd5f76ccc5464b122428034e82d2
kbonney <kirkb1998@gmail.com>
2023-08-31 11:27:32 -0400

    adding a check to see if blade definition changes from what was previously generated

--- src/pynumad/tests/test_blade_io.py
+++ src/pynumad/tests/test_blade_io.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import pickle
 
 from pynumad.objects.blade import Blade
 
@@ -11,12 +12,21 @@ class TestBladeIO(unittest.TestCase):
     def setUpClass(cls):
         cls.xlsxfile = os.path.join(test_data_dir, "blades", "blade.xlsx")
         cls.yamlfile = os.path.join(test_data_dir, "blades", "blade.yaml")
+        
+        with open(os.path.join(test_data_dir,"blades","yaml_blade.pkl"), "rb") as f:
+            cls.yaml_blade_pkl = pickle.load(f)
+            
+        with open(os.path.join(test_data_dir,"blades","excel_blade.pkl"), "rb") as f:
+            cls.excel_blade_pkl = pickle.load(f)
 
     def test_xlsx_blade(self):
         xlsxblade = Blade(self.xlsxfile)
+        assert xlsxblade.definition == self.excel_blade_pkl.definition
+        
 
     def test_yaml_blade(self):
         yamlblade = Blade(self.yamlfile)
+        assert yamlblade.definition == self.yaml_blade_pkl.definition
 
 
 if __name__ == "__main__":
