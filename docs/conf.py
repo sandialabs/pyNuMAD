f39f62e42298fd914281d1087e9d8a0d83203f4e
kbonney <kirkb1998@gmail.com>
2023-10-05 10:54:25 -0400

    adding numfig to conf

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 language = "en"
+numfig = True
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
