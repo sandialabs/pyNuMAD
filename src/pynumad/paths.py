3665a53db45877b38b0cd913bb07b1c8d148d913
Kirk Bonney <47759761+kbonney@users.noreply.github.com>
2023-10-09 14:23:30 -0400

    Misc updates (#30)

--- src/pynumad/paths.py
+++ src/pynumad/paths.py
@@ -7,3 +7,20 @@ DATA_PATH = join(package_dir, "..", "data")
 
 with open(join(package_dir, "software_paths.json"), "rt") as f:
     SOFTWARE_PATHS = json.load(f)
+    
+def set_path(path_label, path):
+    """_summary_
+
+    Parameters
+    ----------
+    path_label : str
+        Label of the path to be set.
+            - "cubit"
+            - "cubit_enhancements"
+            - "ansys"
+    path : str
+        Value to set label as
+    """
+    SOFTWARE_PATHS[path_label] = path
+    with open(join(package_dir, "software_paths.json"), "wt") as f:
+        json.dump(SOFTWARE_PATHS, f, indent=4)
